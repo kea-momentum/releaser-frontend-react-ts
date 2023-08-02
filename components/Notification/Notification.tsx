fce63d0eddb6c203212ebd61255a34c9a0036d9f
gnryu <gnnryu@gmail.com>
2023-08-03 01:31:11 +0900

    REL-87 feature: Make Notification file

---
+++ components/Notification/Notification.tsx
@@ -0,0 +1,10 @@
+import * as S from "./Notification.styled";
+import React, { Fragment } from "react";
+
+export default function Notification() {
+    return (
+        <Fragment>
+            Notification
+        </Fragment>
+    );
+}
