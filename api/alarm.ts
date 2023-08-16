3a533b885b84820375ff35258161eb90d4563616
gnryu <gnnryu@gmail.com>
2023-08-16 17:19:33 +0900

    REL-87 feat: Alarm proxy

--- api/alarm.ts
+++ api/alarm.ts
@@ -1,16 +1,40 @@
-import { privateApi } from "./getToken";
+// import { privateApi } from "./getToken";
+import {Client, Message} from "@stomp/stompjs";
+import SockJS from "sockjs-client";
 import { Response } from "@/types";
 
-export const alarmHistory = async (
-    page: number,
-    size: number
-): Promise<Response<any>> => {
-    try {
-        const response = await privateApi.get(
-            `/api/notifications?page=${page}&size=${size}`
-        );
-        return response.data;
-    } catch(error) {
-        throw error;
-    }
+export const alarmHistory = ({email, page, size, setMessageCallback}: {
+    email: string;
+    page: number;
+    size: number;
+    setMessageCallback: (message: string) => void;
+}) => {
+    const sockJs = new SockJS(`${process.env.NEXT_PUBLIC_API_URL}/notification`);
+    const stompClient = new Client({
+        webSocketFactory: () => sockJs,
+        debug: (str) => {
+            console.log(str);
+        },
+    });
+
+    stompClient.onConnect = (frame) => {
+        const token = window.sessionStorage.getItem("accessToken");
+        const headers = {Authorization: `Bearer ${token}`};
+        const subscription = stompClient.subscribe(`/queue/releaser.user.${email}`, (message) => {
+            const messageBody = JSON.parse(message.body);
+            setMessageCallback(messageBody);
+            console.log(">>> [TEST] Notification Message\n", messageBody);
+            message.ack();
+        }, headers);
+
+        return () => {
+            subscription.unsubscribe();
+            if(stompClient) {
+                stompClient.deactivate();
+                console.log("Disconnect");
+            }
+        };
+    };
+    
+    stompClient.activate();
 };
--- components/Notification/Notification.tsx
+++ components/Notification/Notification.tsx
@@ -1,17 +1,57 @@
 import * as S from "./Notification.styled";
 import React, {useEffect, useState, useRef} from "react";
-import {Client, Message} from "@stomp/stompjs";
-import SockJS from "sockjs-client";
+// import {Client, Message} from "@stomp/stompjs";
+// import SockJS from "sockjs-client";
 import { alarmHistory } from "@/api/alarm";
 
 export default function Notification() {
-    const stompClient = useRef<Client | null>(null);
+    // const stompClient = useRef<Client | null>(null);
     const [email, setEmail] = useState<string>("gnaryu@naver.com"); // FIXME: 하드코딩
-    const [message, setMessage] = useState<String>("");
+    const [message, setMessage] = useState<string>("");
 
+    // useEffect(() => {
+    //     const sockJs = new SockJS(`${process.env.NEXT_PUBLIC_API_URL}/api/notifications?page=0&size=5`);
+    //     stompClient.current = new Client({
+    //         webSocketFactory: () => sockJs,
+    //         debug: (str: string) => {
+    //             console.log(str);
+    //         },
+    //     });
+
+    //     stompClient.current.onConnect = (frame) => {
+    //         const subscription = stompClient.current?.subscribe(`/queue/releaser.user.${email}`, (message: Message) => {
+    //             console.log("In subscription");
+    //             const messageBody = JSON.parse(message.body);
+    //             console.log(">>> Notification\n", messageBody);
+    //             setMessage(JSON.stringify(messageBody));
+    //             message.ack();
+    //         });
+
+    //         return () => {
+    //             subscription?.unsubscribe();
+    //             if(stompClient.current) {
+    //                 stompClient.current.deactivate();
+    //                 console.log("Disconnect");
+    //             }
+    //         };
+    //     };
+
+    //     stompClient.current.activate();
+    // }, []);
+    // useEffect(() => {
+    //     console.log(">>> Alarm Message\n", message);
+    // }, [message]);
+
+
+    useEffect(() => { // FIXME: sockjs 분리
+        console.log("왜 안 돼");
+        alarmHistory({email, page: 0, size: 5, setMessageCallback: (receivedMessage: string) => {
+            setMessage(JSON.stringify(receivedMessage));
+        }});
+    }, [email]);
     useEffect(() => {
-        const sockJs = new SockJS('');
-    }, []);
+        console.log(">>> Notification Message\n", message);
+    }, [message]);
 
     return (
         <S.Wrapper>
