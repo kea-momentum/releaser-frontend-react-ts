c9ba37d6734a3c3b740c5d9bb6ef43f8fe7ed6ae
gnryu <gnnryu@gmail.com>
2023-08-18 07:38:45 +0900

    REL-87 feat: Alarm modal

--- components/Notification/Notification.tsx
+++ components/Notification/Notification.tsx
@@ -1,21 +1,30 @@
 import * as S from "./Notification.styled";
 import React, {useEffect, useState} from "react";
+import { alarmHistoryContent } from "@/types/Alarm";
 
-export default function Notification() {
+export default function Notification({data}: {data: alarmHistoryContent}) {
+    const [formattedDate, setFormattedDate] = useState<string>("");
+    useEffect(() => {
+        console.log(">>> DATA\n", data);
+        const date = new Date(data.date);
+        const formatted = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
+        setFormattedDate(formatted);
+    }, [data]);
+
     return (
         <S.Wrapper>
             <S.LogoSection>
                 <S.ImgWrapper>
-                    
+                    <img src={data.projectImg} alt="Project Logo" />
                 </S.ImgWrapper>
             </S.LogoSection>
             <S.MainInfoSection>
-                <S.ProjectInfo>[프로젝트명]</S.ProjectInfo>
-                <S.NotificationInfo>릴리즈 노트의 배포 동의 여부를 선택해주세요.</S.NotificationInfo>
+                <S.ProjectInfo>[{data.projectTitle}]</S.ProjectInfo>
+                <S.NotificationInfo>{data.message}</S.NotificationInfo>
             </S.MainInfoSection>
             <S.RightInfoSection>
-                <S.NotificationType>Release Note</S.NotificationType>
-                <S.DateInfo>2023.6.27</S.DateInfo>
+                <S.NotificationType>{data.type}</S.NotificationType>
+                <S.DateInfo>{formattedDate}</S.DateInfo>
             </S.RightInfoSection>
         </S.Wrapper>
     );
